693cd948edbe8bc70dfa50c76d1562b49bb77528
0leum <ritam.d3v@gmail.com>
2024-03-23 22:10:21 +0530

    minor updates

--- src/index.js
+++ src/index.js
@@ -32,20 +32,20 @@ connectDB()
 
 // const app = express()
 
-//     ; (async () => {
-//         try {
-//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//             app.on("Error", (error) => {
-//                 console.log("ERRRR: ", error)
-//                 throw error
-//             })
+//; (async () => {
+//  try {
+//    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//    app.on("Error", (error) => {
+//        console.log("ERRRR: ", error)
+//        throw error
+//    })
 
-//             app.listen(process.env.PORT, () => {
-//                 console.log(`App is listening on ${process.env.PORT}`)
-//             })
-//         }
-//         catch (error) {
-//             console.error("ERROR: ", error)
-//             throw error
-//         }
-//     })()
+//    app.listen(process.env.PORT, () => {
+//        console.log(`App is listening on ${process.env.PORT}`)
+//    })
+//  }
+//  catch (error) {
+//    console.error("ERROR: ", error)
+//    throw error
+//  }
+//})()
